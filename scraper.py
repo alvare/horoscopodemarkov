ea94a8f643591cabfdde721f8ac5c5b9975ac819
alvare <welcometothechango@gmail.com>
2014-01-28 14:41:07 -0300

    better scrapping

--- scraper.py
+++ scraper.py
@@ -3,12 +3,12 @@ from datetime import date, timedelta
 import requests
 import codecs
 
-results = []
 signos = ["aries", "tauro", "geminis", "cancer", "leo", "virgo", "libra" ,"escorpio", "sagitario", "capricornio", "acuario", "piscis"]
 
 for signo in signos:
+  results = []
   print signo
-  for strdate in [(date.today() - timedelta(a)).strftime("%d%m%Y") for a in range(20)]:
+  for strdate in [(date.today() - timedelta(a) - timedelta(22)).strftime("%d%m%Y") for a in range(20)]:
     try:
       r = requests.get("http://horoscopo.abc.es/signos-zodiaco-" + signo + "/prediccion-" + strdate + ".html", timeout=10)
 
@@ -21,5 +21,6 @@ for signo in signos:
     except:
       pass
 
-  with codecs.open("data/" + signo + ".data", "w", "utf-8") as f:
+  with codecs.open("data/" + signo + ".data", "a", "utf-8") as f:
+    f.write(";;;")
     f.write(";;;".join(results))
